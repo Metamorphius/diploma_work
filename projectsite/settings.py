966988ab449b8498a8e770b3aefd460ee6cc9fce
Metamorph <koblabcorp@gmail.com>
2022-04-03 02:00:25 +0300

    Add DigitalCurrency model

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'projectsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'website_bd',
+        'USER' : 'postgres',
+        'PASSWORD' : 'postgres',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class  DigitalCurrency(models.Model):
+    ticker = models.CharField(max_length=15, unique=True)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=15, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['ticker']
+
