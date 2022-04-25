dfa9f03a9ec2a30f7506f7ed27ef2c23d06f0ff4
Metamorph <koblabcorp@gmail.com>
2022-04-25 23:54:16 +0300

    Added stock form

--- website/forms.py
+++ website/forms.py
@@ -4,3 +4,6 @@ from .models import *
 
 class CryptForm(forms.Form):
     ticker = forms.ModelChoiceField(queryset=DigitalCurrency.objects.only('ticker'))
+
+class StockForm(forms.Form):
+    ticker = forms.ModelChoiceField(queryset=Stock.objects.only('ticker'))
